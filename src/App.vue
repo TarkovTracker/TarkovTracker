838bc6d74ccd5e382ce847ccf7e926a5f2c63cc7
Thaddeus Bond <thaddeus@thaddeus.io>
2021-12-11 16:06:20 -0500

    Add maintenance mode remote config setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <v-fade-transition mode="out-in">
-    <router-view />
+    <template v-if="maintenance_mode == 'true'">
+      <div class='maintenance-mode'>
+        <img src="https://tarkovtracker.io/img/tarkovtrackerlogo-light.png" class="maintenance-logo">
+        <h1>TarkovTracker is in maintenace mode</h1>
+        <p>We are working on upgrading a backend system which necessitates a short downtime.</p>
+        <p>You can check the <a href="https://discord.gg/zeAP4Ng">TarkovTracker Discord server</a> for the latest updates</p>
+      </div> 
+    </template>
+    <template v-else>
+      <router-view />
+    </template>
   </v-fade-transition>
 </template>
 
 <script>
   // Styles
   import '@/styles/overrides.sass'
+  import fireapp from './fireapp.js'
 
   export default {
     name: 'TarkovTracker',
+    data () {
+      return {
+        maintenance_mode: false,
+      }
+    },
     metaInfo: {
       title: 'Dashboard',
       titleTemplate: '%s | TarkovTracker',
@@ -18,6 +34,16 @@
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+    },
+    mounted() {
+      const remoteConfig = fireapp.remoteConfig();
+      
+      this.maintenance_mode = remoteConfig.getValue("maintenance_mode")._value;
+      remoteConfig.fetchAndActivate()
+      .then(() => {
+        this.maintenance_mode = remoteConfig.getValue("maintenance_mode")._value;
+        console.log(this.maintenance_mode)
+      })
     },
     changed (metaInfo) {
       this.$analytics.setCurrentScreen(metaInfo.title)
@@ -29,3 +55,14 @@
     },
   }
 </script>
+<style lang="sass">
+.maintenance-mode
+ color: #FFFFFF
+ text-align: center
+.maintenance-logo
+  display:block
+  margin-left:auto
+  margin-right:auto
+  margin-top:64px
+  margin-bottom:16px
+</style>
--- src/fireapp.js
+++ src/fireapp.js
@@ -4,10 +4,17 @@ import 'firebase/firestore'
 import 'firebase/functions'
 import 'firebase/analytics'
 import 'firebase/auth'
+import 'firebase/remote-config'
 
 // Get a Firestore instance
 const fireapp = firebase
   .initializeApp(firebaseConfig)
+
+const remoteConfig = fireapp.remoteConfig();
+remoteConfig.settings.minimumFetchIntervalMillis = 1800000;
+remoteConfig.defaultConfig = {
+  "maintenance_mode": false
+};
 
 if (window.location.hostname === 'localhost') {
   fireapp.firestore().useEmulator('localhost', 8081)
