c4032b29e2f80c2b5bce1be19f0f9506224d077d
Thaddeus Bond <thaddeus@thaddeus.io>
2022-06-04 13:28:39 -0400

    Optimize for CI workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '2i9t26',
   e2e: {
-    baseUrl: 'http://localhost:8080/',
+    baseUrl: 'http://localhost:5000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
