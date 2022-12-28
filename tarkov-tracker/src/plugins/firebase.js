be4e84ebc5eb7c4e7a12ef53b37bb77e97d319e7
Thaddeus Bond <thaddeus@thaddeus.io>
2022-12-28 08:23:46 -0500

    Add hostname check for tt env

--- tarkov-tracker/src/plugins/firebase.js
+++ tarkov-tracker/src/plugins/firebase.js
@@ -5,16 +5,38 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/functions'
 import 'firebase/compat/auth'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCRgDK1U5wjeQA8RbRFTAj_jprsG4hF0zw",
-  authDomain: "tarkovtracker-production.firebaseapp.com",
-  databaseURL: "https://tarkovtracker-production-default-rtdb.firebaseio.com",
-  projectId: "tarkovtracker-production",
-  storageBucket: "tarkovtracker-production.appspot.com",
-  messagingSenderId: "140836645499",
-  appId: "1:140836645499:web:2253dd97b8224a898b90de",
-  measurementId: "G-ZH4YVDFWTJ"
-};
+const { hostname } = new URL(document.location.href)
+
+const firebaseConfig = (hostname == 'tarkovtracker.io')
+  ? {
+    apiKey: 'AIzaSyCRgDK1U5wjeQA8RbRFTAj_jprsG4hF0zw',
+    authDomain: 'auth.tarkovtracker.io',
+    projectId: 'tarkovtracker-production',
+    storageBucket: 'tarkovtracker-production.appspot.com',
+    messagingSenderId: '140836645499',
+    appId: '1:140836645499:web:2253dd97b8224a898b90de',
+    measurementId: 'G-ZH4YVDFWTJ'
+  }
+  : (hostname == 'tarkovtracker-staging.web.app')
+    ? {
+      apiKey: 'AIzaSyARSzfwWQlvRfyEVVoH6psgnswpxY-qo70',
+      authDomain: 'tarkovtracker-staging.firebaseapp.com',
+      projectId: 'tarkovtracker-staging',
+      storageBucket: 'tarkovtracker-staging.appspot.com',
+      messagingSenderId: '944174466132',
+      appId: '1:944174466132:web:eec147253f3d13ff9b99c1',
+      measurementId: 'G-WPT5B7M897'
+    }
+    : {
+      apiKey: "AIzaSyDzod-ZcUKOmlYNChDAeGWhoatPt6niPu0",
+      authDomain: "tarkovtracker-next.firebaseapp.com",
+      projectId: "tarkovtracker-next",
+      storageBucket: "tarkovtracker-next.appspot.com",
+      messagingSenderId: "260245547337",
+      appId: "1:260245547337:web:e2821d280026b1af539298",
+      measurementId: "G-JYJTN74JNW"
+    }
+
 
 // Initialize Firebase
 const fireapp = firebase.initializeApp(firebaseConfig);
