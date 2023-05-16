9fb005f339db8a74ca7e1a120e357133d72e3041
Thaddeus Bond <thaddeus@thadddeusbond.com>
2023-05-16 00:23:32 -0400

    Fix search bar sizing

--- tarkov-tracker/src/pages/NeededItems.vue
+++ tarkov-tracker/src/pages/NeededItems.vue
@@ -1,87 +1,43 @@
 <template>
   <tracker-tip tip="neededitems"></tracker-tip>
   <v-container>
-    <v-row v-if="loading || hideoutLoading" justify="center">
-      <v-col cols="12" align="center">
-        <v-progress-circular
-          indeterminate
-          color="secondary"
-          class="mx-2"
-        ></v-progress-circular>
-        {{ $t("page.neededitems.loading") }} <refresh-button />
-      </v-col>
-    </v-row>
     <v-row justify="center" align="center" dense>
       <v-col cols="12" sm="12" md="3" lg="3">
         <!-- Primary views (all, maps, traders) -->
         <v-card>
-          <v-tabs
-            v-model="activeNeededView"
-            bg-color="accent"
-            slider-color="secondary"
-            align-tabs="center"
-            show-arrows
-          >
-            <v-tab
-              v-for="(view, index) in neededViews"
-              :key="index"
-              :value="view.view"
-              :prepend-icon="view.icon"
-            >
+          <v-tabs v-model="activeNeededView" bg-color="accent" slider-color="secondary" align-tabs="center" show-arrows>
+            <v-tab v-for="(view, index) in neededViews" :key="index" :value="view.view" :prepend-icon="view.icon">
               {{ view.title }}
             </v-tab>
           </v-tabs>
         </v-card>
       </v-col>
       <v-col cols="12" sm="11" md="8" lg="8">
-        <v-text-field
-          v-model="itemFilterNameText"
-          label="Search by item name"
-          variant="solo"
-          hide-details
-        ></v-text-field>
+        <v-text-field v-model="itemFilterNameText" label="Search by item name" variant="solo" hide-details
+          density="comfortable"></v-text-field>
       </v-col>
       <v-col cols="1">
         <v-dialog v-model="settingsDialog">
           <template #activator="{ props }">
-            <v-btn
-              v-bind="props"
-              variant="tonal"
-              style="width: 100%; height: 48px"
-            >
+            <v-btn v-bind="props" variant="tonal" style="width: 100%; height: 48px">
               <v-icon>mdi-cog</v-icon>
             </v-btn>
           </template>
           <v-row class="justify-center">
             <v-col cols="auto">
-              <v-card
-                :title="$t('page.neededitems.options.title')"
-                style="width: fit-content"
-              >
+              <v-card :title="$t('page.neededitems.options.title')" style="width: fit-content">
                 <v-card-text>
                   <v-container class="ma-0 pa-0">
                     <v-row dense>
                       <v-col cols="12">
-                        <v-switch
-                          v-model="hideFIR"
-                          :label="$t(hideFIRLabel)"
-                          inset
-                          true-icon="mdi-eye-off"
-                          false-icon="mdi-eye"
-                          :color="hideFIRColor"
-                          hide-details
-                          density="compact"
-                        ></v-switch>
+                        <v-switch v-model="hideFIR" :label="$t(hideFIRLabel)" inset true-icon="mdi-eye-off"
+                          false-icon="mdi-eye" :color="hideFIRColor" hide-details density="compact"></v-switch>
                       </v-col>
                     </v-row>
                     <v-row justify="end">
                       <v-col cols="12" md="6">
-                        <v-btn
-                          color="primary"
-                          block
-                          @click="settingsDialog = false"
-                          >{{ $t("page.neededitems.options.close") }}</v-btn
-                        >
+                        <v-btn color="primary" block @click="settingsDialog = false">{{
+                          $t("page.neededitems.options.close") }}</v-btn>
                       </v-col>
                     </v-row>
                   </v-container>
@@ -92,21 +48,19 @@
         </v-dialog>
       </v-col>
     </v-row>
+    <v-row v-if="loading || hideoutLoading" justify="center">
+      <v-col cols="12" align="center">
+        <v-progress-circular indeterminate color="secondary" class="mx-2"></v-progress-circular>
+        {{ $t("page.neededitems.loading") }} <refresh-button />
+      </v-col>
+    </v-row>
     <v-row v-show="activeNeededView == 'all' || activeNeededView == 'tasks'">
-      <needed-item-card
-        v-for="(neededItem, itemIndex) in neededTaskItems"
-        :key="itemIndex"
-        :need="neededItem"
-        class="my-1"
-      />
+      <needed-item-card v-for="(neededItem, itemIndex) in neededTaskItems" :key="itemIndex" :need="neededItem"
+        class="my-1" />
     </v-row>
     <v-row v-show="activeNeededView == 'all' || activeNeededView == 'hideout'">
-      <needed-item-card
-        v-for="(neededItem, itemIndex) in neededHideoutItems"
-        :key="itemIndex"
-        :need="neededItem"
-        class="my-1"
-      />
+      <needed-item-card v-for="(neededItem, itemIndex) in neededHideoutItems" :key="itemIndex" :need="neededItem"
+        class="my-1" />
     </v-row>
   </v-container>
 </template>
