590a274fd2a2b12708bb96c7d6efa8c1e497dce8
Thaddeus Bond <thaddeus@thadddeusbond.com>
2023-08-09 23:11:45 -0400

    Update dashboard notices

--- tarkov-tracker/src/pages/TrackerDashboard.vue
+++ tarkov-tracker/src/pages/TrackerDashboard.vue
@@ -2,6 +2,17 @@
   <tracker-tip tip="welcomett3"></tracker-tip>
 
   <v-container class="mt-2">
+    <v-alert density="compact" type="alert" title="Wipe Update">Escape From Tarkov 0.13.5 is here -
+      progress will be cleared automatically for logged in users. If you are using TarkovTracker without signing in, you
+      can reset your progress through the settings. Changes to quests and hideout upgrades will be pulled automatically
+      from <a href='http://tarkov.dev/'>tarkov.dev</a> as they are discovered and confirmed.</v-alert>
+    <v-alert density="compact" type="success" title="Automatic Quest Completion" class="mb-6 mt-3">Thanks to the
+      wonderful folks over at <a href='http://tarkov.dev/'>tarkov.dev</a>, its easier than ever to keep your TarkovTracker
+      progress up to date with your in-game progress. They've worked on a new open-source tool which watches the log files
+      for the game for messages about quest completions. If you link a TarkovTracker API token with the tool, it can
+      automatically mark off the quest for you via the free TarkovTracker API. Check out the project on <a
+        href='https://github.com/the-hideout/TarkovMonitor'>GitHub</a> and download it from the project's <a
+        href='https://github.com/the-hideout/TarkovMonitor/releases/latest'>Releases</a></v-alert>
     <v-row justify="center">
       <v-col cols="12" sm="8" md="6" lg="4" xl="3">
         <tracker-stat icon="mdi-progress-check">
