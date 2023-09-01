fc6b96b185c030e0e91ffc44cc68af881ca39023
whoandozco <juandozco@hotmail.com>
2023-09-01 20:27:16 +0900

    clean: remove console.log

--- tarkov-tracker/src/pages/HideoutList.vue
+++ tarkov-tracker/src/pages/HideoutList.vue
@@ -110,8 +110,6 @@ const visibleStations = computed(() => {
   let hideoutStationList = JSON.parse(JSON.stringify(hideoutStations.value));
   let visibleStations = progressStore.visibleStations;
 
-  console.log(visibleStations);
-
   //Display all upgradeable stations
   if (activePrimaryView.value === "available")
     return Object.values(visibleStations).filter(
